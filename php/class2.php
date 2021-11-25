10a1191a8dacc3946bf8e2a0813cbeb053427cc6
EC2 Default User <ec2-user@ip-172-31-38-161.ap-northeast-1.compute.internal>
2021-11-25 13:57:05 +0000

    first commit

---
+++ php/class2.php
@@ -0,0 +1,2 @@
+
+use Illuminate\Http\Request;
---
+++ php/restaurants_searcher.php
@@ -0,0 +1,41 @@
+<?php
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
+$KEYID = "0a4c76598b2c7d98";
+$HIT_PER_PAGE = 100;
+$PREF = "Z011";
+$FREEWORD = "渋谷駅";
+$FORMAT = "json";
+
+$PARAMS = ["key"=> $KEYID, "count"=>$HIT_PER_PAGE, "pref"=>$PREF, "keyword"=>$FREEWORD, "format"=>$FORMAT];
+
+function write_data_to_csv($params){
+    
+    $restaurants = [];
+    $client = new Client();
+    try{
+        $json_res = $client->request('GET', "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/", ['query' => $params])->getBody();
+    }catch(Exception $e){
+        return print("エラーが発生しました。APIのURLを確認してください。");
+    }
+    $response = json_decode($json_res,true);
+    
+    if(isset($response["results"]["error"])){
+        return(print("エラーが発生しました。APIのパラメータを確認してください。"));
+    }
+    
+    foreach($response["results"]["shop"] as $restaurant){
+        $restaurant_name = $restaurant["name"];
+        $restaurants[] = $restaurant_name;
+    }
+    $handle = fopen("restaurants_list.csv", "wb");
+    fputcsv($handle, $restaurants);
+    fclose($handle);
+    return print_r($restaurants);
+}
+
+write_data_to_csv($PARAMS);
+
+?>
